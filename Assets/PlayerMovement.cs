b24b89e43553c4bb698807d3e0b1b53a68202b6e
Matheus Salomão <ms019042@gmail.com>
2021-05-01 20:35:46 -0300

    movimento do player

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class PlayerMovement : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public Rigidbody rb;
+
+    public float forwardForce = 800f;
+    public float sidewaysForce = 600f;
+
+    void FixedUpdate() {   
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if(Input.GetKey("d")){
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+
+        }
+        if(Input.GetKey("a")){
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+
+        }
     }
 }
