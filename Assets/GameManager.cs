d7493e2564bc66e6be57c0e34339bb8d7f1e01f4
Matheus Salomão <ms019042@gmail.com>
2021-05-04 19:26:19 -0300

    GameOver, Complete Level e Animação da UI

---
+++ Assets/GameManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    bool gameHasEnded = false;
+
+    public float restartDalay = 1f;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel () {
+        completeLevelUI.SetActive(true);
+    }
+
+    public void GameOver () {
+        if(!gameHasEnded){
+            gameHasEnded = true;
+            Invoke("Restart", restartDalay);
+        }
+    }
+
+    void Restart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
